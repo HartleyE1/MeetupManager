4a25c3d03581521cccc7f651c477191648667964
HartleyE1 <67687015+HartleyE1@users.noreply.github.com>
2020-07-01 20:06:31 +1000

    Add files via upload

---
+++ LogIn.js
@@ -0,0 +1,22 @@
+function logIn() {
+	var username = document.getElementsByTagName('username').value
+	var password = document.getElementsByTagName('password').value
+	
+	if (username = "OliverK") {
+		if (password = "PinkFrogs") {
+			window.open("info.html","_self")
+		}
+	}
+
+	if (username = "JackH") {
+		if (password = "OrangeCups") {
+			window.open("info.html","_self")
+		}
+	}
+
+	if (username = "FabianE") {
+		if (password = "AboveBellow") {
+			window.open("info.html","_self")
+		}
+	}
+}
